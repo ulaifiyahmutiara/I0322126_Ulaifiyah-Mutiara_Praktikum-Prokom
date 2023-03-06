59242207d651e98986905dc817d34c146f882eb3
tita <ulaifiyahmutiara@student.uns.ac.id>
2023-03-06 11:44:59 +0700

    commit 1

---
+++ contoh.py
@@ -0,0 +1,9 @@
+
+print('nama saya adalah tiara')
+print("nama saya adalah tiara")
+print("nama saya adalah tuara")
+print("saya adalah tiara")
+nama_depan = "tiara"
+print(nama_depan)
+nama_blk = "mutiara"
+
