053450ee5c57334de184154f5ce1104c0e0cafc5
tita <ulaifiyahmutiara@student.uns.ac.id>
2023-03-12 18:01:18 +0700

    exercise-01

---
+++ exercise-01.py
@@ -0,0 +1,23 @@
+#tugas praktikum prokom
+
+tulisan_1 = 'Ulaifiyah Mutiara'
+tulisan_2 = 'I0322126'
+tulisan_3 = 'Kota Tangerang'
+
+print('=========================')
+
+print('Nama           = ', tulisan_1)
+print('NIM            = ', tulisan_2)
+print('Tempat tinggal = ', tulisan_3)
+
+print('''
+Saya lahir dan besar di Kota Tangerang. Tangerang merupakan kawasan industri yang menjadi daerah penyokong DKI Jakarta dan merupakan kota yang padat. Di daerah tempat saya tinggal, terdapat sungai yang menjadi salah satu ciri khas Kota Tangerang, yaitu Sungai Cisadane.
+''')
+
+print('''
+Setelah lulus dari Teknik Industri, hal yang ingin saya lakukan adalah dengan melanjutkan studi ke jenjang s2. Setelah selesai mengambil studi s2, harapan saya adalah bisa mempunyai pekerjaan yang sebanding dengan apa yang saya usahakan selama ini lewat pendidikan dan bisa mengaplikasikan ilmu yang saya dapat agar berguna bagi masyarakat sekitar.
+''')
+
+print('=======================')
+
+
